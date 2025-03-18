11b9aed32a13da5e7f71c657c138531952988014
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-03-18 17:10:29 +0100

    adding interactive counters

--- index.js
+++ index.js
@@ -2,13 +2,25 @@
 //import fx from 'fireworks';
 
 //déclaration
+//variables
 let flippedCard = 0;
 let paires = 0;
 let cardAId = "";
 let cardBId = "";
+let movesCurrent = 0;
+let movesEnd = 0;
+let playOn = true;
+let isFirst = true;
+let startTime;
+let endTime;
+let currentTime = 0;
+//selectors
 const allCards = document.querySelectorAll(".img-card");
 //const container = document.querySelector('.container')
-
+const selectCurrentMoves = document.querySelector(".current-moves")
+const selectCurrentTime = document.querySelector(".current-time")
+const selectBestMoves = document.querySelector(".best-moves")
+const selectBestTime = document.querySelector(".best-time")
 
 //fonctions
 function flip(card) {
@@ -32,21 +44,23 @@ function flip(card) {
 }
 
 function compare() {
+    movesCurrent++;
+    selectCurrentMoves.innerHTML = movesCurrent;
     if (cardAId === cardBId) {
         paires++;
-       /*fx({
-            x: 50, // required
-            y: 50 // required
-
-          })*/
+        /*fx({
+             x: 50, // required
+             y: 50 // required
+ 
+           })*/
         const winPaire = document.querySelectorAll(".flipped")
-        for (let winCard of winPaire) {           
+        for (let winCard of winPaire) {
             winCard.setAttribute("class", "img-card no-display");
             winCard.setAttribute("id", "");
         }
     }
     else {
-        const badA = document.querySelector("#cardA");        
+        const badA = document.querySelector("#cardA");
         const badB = document.querySelector("#cardB");
         badA.setAttribute("id", "")
         badB.setAttribute("id", "")
@@ -54,16 +68,43 @@ function compare() {
         badB.classList.remove("flipped");
         flip(badA);
         flip(badB);
-    }   
-    flippedCard = 0;   
+    }
+    flippedCard = 0;
     cardAId = "";
     cardBId = "";
+}
+
+function displayTime(timeInMS) {
+    let hh = Math.floor(timeInMS / 3600000);
+    let mm = "";
+    let ss = "";
+    if (Math.floor((timeInMS % 3600000) / 60000) < 10) {
+        mm = `0${Math.floor((timeInMS % 3600000) / 60000)}`
+    }
+    else {
+        mm = `${Math.floor((timeInMS % 3600000) / 60000)}`
+    }
+    if (Math.floor((timeInMS % 3600000) % 60000 / 1000) < 10) {
+        ss = `0${Math.floor((timeInMS % 3600000) % 60000 / 1000)}`
+    }
+    else {
+        ss = `${Math.floor((timeInMS % 3600000) % 60000 / 1000)}`
+    }
+    return `${hh}:${mm}:${ss}`;
 }
 
 //run
 for (const card of allCards) {
     card.addEventListener('click', function () {
-        if (card.classList.contains("flipped")) { }
+        if (isFirst) {
+            startTime = Date.now();
+            setInterval(() => {
+                currentTime = Date.now() - startTime;
+                selectCurrentTime.innerHTML = displayTime(currentTime);
+            }, 1000)
+            isFirst = false;
+        }
+        if (card.classList.contains("flipped") || !playOn) { }
         else if (flippedCard === 2) { }
         else {
             flip(card);
@@ -73,17 +114,21 @@ for (const card of allCards) {
             if (cardAId === "") {
                 card.setAttribute("class", "flipped" + " " + card.getAttribute("class"));
                 card.setAttribute("id", "cardA")
-                cardAId = (card.getAttribute("class"))                
+                cardAId = (card.getAttribute("class"))
             }
             else if (cardBId === "") {
                 card.setAttribute("class", "flipped" + " " + card.getAttribute("class"));
                 card.setAttribute("id", "cardB")
                 cardBId = (card.getAttribute("class"))
-            }            
-            if (flippedCard === 2) {                
+            }
+            if (flippedCard === 2) {
                 setTimeout(compare, 1500);
 
             }
+        }
+        if (paires === 5) {
+            playOn = false;
+            movesEnd = movesCurrent
         }
     })
 }
